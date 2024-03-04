04fade9c706d029b408ff33edc06bbe3e2b5f5e2
amasdeogratias <deogratiasprog@gmail.com>
2024-03-04 23:07:32 +0300

    Added roles and users and made user role assignment

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->delete();
+        $superAdmin = User::firstOrCreate([
+            'id'                => 1,
+            'name'              => 'Deo Amas',
+            'email'             => 'super@admin.com',
+            'password'          => Hash::make('password'),
+            'email_verified_at' => now(),
+        ]);
+
+        $superAdmin->assignRole('super-admin');
+        $superAdmin->save();
+
+        $admin = User::firstOrCreate([
+            'id'                => 2,
+            'name'              => 'John Doe',
+            'email'             => 'admin@example.com',
+            'password'          => Hash::make('password'),
+            'email_verified_at' => now(),
+        ]);
+
+        $admin->assignRole('admin');
+
+        $manufacture = User::firstOrCreate([
+            'id'                => 3,
+            'name'              => 'Abuu Ismail',
+            'email'             => 'abuu@gmail.com',
+            'password'          => Hash::make('password'),
+            'email_verified_at' => now(),
+        ]);
+
+        $manufacture->assignRole('manufacture');
+
+        $retailer = User::firstOrCreate([
+            'id'                => 4,
+            'name'              => 'David Wakarungi',
+            'email'             => 'david@gmail.com',
+            'password'          => Hash::make('password'),
+            'email_verified_at' => now(),
+        ]);
+
+        $retailer->assignRole('retailer');
+    }
+}
